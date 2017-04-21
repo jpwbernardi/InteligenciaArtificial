18946edaa0c44abdd477d8490e0a697daf8b5019
João Winckler <jpwbernardi@hotmail.com>
2017-04-21 19:43:05 -0300

    bug fixed

--- main.cpp
+++ main.cpp
@@ -10,13 +10,16 @@ using namespace std;
 
 #define MAX 50  //Quantidade máxima de professores, turmas, disciplinas
 #define ERR 3  //Quantidade de erros aceitados
-#define TAM 10 //Tamanho da população
+#define TOLERANCE 3
+#define TAM 30 //Tamanho da população
 #define INF 112345678
 #define QTDBILHETES 10
 #define QTDTURMAS 9
-#define TIMEMAX 8
+#define TIMEMAX 20
 #define LIVRE -1
 typedef pair< int , vector<int> > iv;
+
+int fim = 0; // Variavel que controla quantas vezes podemos retentar o processo
 
 map<string, int> professores, turmas, disciplina;
 vector<int> rejeitados[MAX], horarios[MAX];
@@ -49,7 +52,7 @@ struct individuo {
   individuo () { memset(this->grade, LIVRE, sizeof(this->grade)); vivo = 1; erros = 0; }
   individuo (set<int> m) {
     memset(this->grade, LIVRE, sizeof(this->grade)); vivo = 1; erros = 0;
-    for (auto const& i : m) this->addmateria(i);
+    for (auto const& i : m) this->addmateria(i); //Deveria ser mais aleatório
   }
 
   //retorna true se há uma restrição nesse horário
@@ -88,21 +91,30 @@ struct individuo {
   }
 
   void addmateria(int m) {
-    int i, j, periodo, turno, count = 2; //Devemos adicionar duas vezes a matéria na grade
-    int possivel[2], p = 0;
-    for (int y = 0; y < (int)infdisc[m].size(); y++) {
+    int i, j, periodo, turno, count, p;
+    int possivel[2];
+    this->materias.insert(m);
+    for (int y = 0; y < (int) infdisc[m].size(); y++) {
+      p = 0; count = 2; //Devemos adicionar duas vezes a matéria na grade
       periodo = infdisc[m][y].first;
       vector<int> prof = infdisc[m][y].second;
-      for (int k = 0; k < (int)horarios[periodo].size(); k++) {
+      for (int k = 0; k < (int) horarios[periodo].size() && count; k++) {
         turno = horarios[periodo][k];
         i = turno / 5; j = turno % 5;
+        //        printf("1\n");
         if (!this->proflivre(prof, turno)) continue;
+        //        printf("2\n");
         if (this->grade[periodo][i][j] == LIVRE && profgosta(prof, turno)) {
+          //          printf("3\n");
           this->grade[periodo][i][j] = m; count--;
+          //printf("4\n");
         } else if (!this->restrito(prof, turno) && p < 2) possivel[p++] = turno;
+        //        printf("5\n");
       }
+      //      printf("6\n");
       if (count > p) this->vivo = 0; //Não foi possivel colocar a matéria :(
-      while (p-- > 0) { this->grade[periodo][possivel[p] / 5] [possivel[p] % 5] = m; this->erros++; }
+      else while (p-- > 0 && count) { this->grade[periodo][possivel[p] / 5] [possivel[p] % 5] = m; this->erros++; count--; }
+      //printf("7\n");
     }
   }
 
@@ -114,6 +126,7 @@ struct individuo {
     for (auto const& i : b.materias) m.insert(i);
     for (auto const& i : this->materias) m.insert(i);
     for (auto const& i : p.materias) m.erase(i);
+    //printf(">>>>%d %d<<<<<\n", p.materias.size(), m.size());
     for (auto const& i : m) p.addmateria(i);
     return p;
   }
@@ -121,17 +134,16 @@ struct individuo {
   void muta(int p) { // 0 <= P <= 100
     if (p < rand() % 100) return;
     int t = disciplina.size() - this->materias.size();
+    if (t == 0) return;
     t = rand() % t;
-    for (auto const& i: disciplina)
+    //printf("\n");
+    for (auto const& i: disciplina) {
+      //      printf("Uuuuuuuuuuuepaaaaaa %d\n", t);
       if (this->materias.find(i.second) == this->materias.end()) {
-        if (t) { t--; continue; }
-        this->materias.insert(i.second); break;
+        if (t--) continue;
+        this->addmateria(i.second); break;
       }
-    individuo q(this->materias);
-    for (int i = 0; i < 10; i++)
-      for (int j = 0; j < 6; j++)
-        for (int k = 0; k < 5; k++)
-          this->grade[i][j][k] = q.grade[i][j][k];
+    }
   }
 };
 
@@ -141,26 +153,33 @@ void in() {
   int N, M, cont = 0, tmp, prof, tur, mat, novo;
   //Lê os professores
   scanf("%d", &N);
+  printf("Professores:\n");
   while (N--) {
     scanf(" %s %d", nome, &M);
     professores[string(nome)] = cont; prof = cont++;
+    printf("%s: %d", nome, cont - 1);
     while (M--) { scanf("%d", &tmp); rejeitados[prof].push_back(tmp); }
   }
 
   //Lê as turmas/semestres
   scanf("%d", &N); cont = 0;
+  printf("\nSemestres/turmas:\n");
   while (N--) {
     scanf(" %s %d %d", nome, &tmp, &M);
     turmas[string(nome)] = cont; tur = cont++;
+    printf("%s: %d", nome, cont - 1);
     while (M--) { scanf("%d", &tmp); horarios[tur].push_back(tmp);  }
   }
 
   //Lê as disciplinas
+  printf("\nMatérias:\n");
   scanf("%d", &N);
   while (N--) {
     scanf(" %s %d %s %s", materia, &tmp, sem, nome);
-    if (disciplina.find(string(materia)) == disciplina.end())
+    if (disciplina.find(string(materia)) == disciplina.end()) {
       disciplina[string(materia)] = cont++;
+      printf("%s: %d\n", materia, cont);
+    }
     mat = disciplina[string(materia)];
     prof = professores[string(nome)];
     tur = turmas[string(sem)];
@@ -200,43 +219,74 @@ void debug() {
 }
 
 individuo selecao(individuo pop[TAM]) {
+  if (fim == TOLERANCE) return pop[0];
   int bilhetes = 0, count = 0, sorteado;
-  for (int i = 0; i < TAM; i++)
-    if (pop[i].vivo) bilhetes += QTDBILHETES / (pop[i].erros + 1);
-    else { pop[i] = individuo(pop[i].materias);} //Se o inidivuo estava morto, ressuscita ;)
-  sorteado = rand() % bilhetes + 1;
   for (int i = 0; i < TAM; i++) {
-    count += QTDBILHETES / (pop[i].erros + 1);
+    //printf(">>>erros: %d\n", pop[i].erros);
+    if (pop[i].vivo) bilhetes += QTDBILHETES / (pop[i].erros + 1);
+    else pop[i] = individuo(pop[i].materias); //Se o inidivuo estava morto, ressuscita ;)
+  }
+  if (bilhetes == 0) { fim++; return selecao(pop); }
+  fim = 0; sorteado = rand() % bilhetes + 1;
+  for (int i = 0; i < TAM; i++) {
+    //printf(">>>erros: %d\n", pop[i].erros);
+    if (pop[i].vivo) count += QTDBILHETES / (pop[i].erros + 1);
     if (count >= sorteado) return pop[i];
   }
   return pop[0]; //Nunca vai acontecer, é só para o compilador não reclamar
 }
 
+void print(individuo p) {
+  for (int i = 0; i < QTDTURMAS; i++) {
+    printf("Turma %d:\n", i);
+    for (int j = 0; j < 6; j++,printf("\n"))
+      for (int k = 0; k < 5; k++)
+        printf("%3d", p.grade[i][j][k]);
+  }
+}
+
+
 individuo genetico(int err) {
   individuo pop[TAM], x, y, z;
+  int _contador = 0;
   clock_t inicio = clock();
   while ((double) (clock() - inicio) / CLOCKS_PER_SEC < TIMEMAX) {
+    _contador++;
     individuo novos[TAM];
     for (int i = 0; i < TAM; i++) {
+      //printf("A\n");
       x = selecao(pop);
+      if (fim != 0) { printf("Não foi possível determinar um horário.\n"); return pop[0]; }
       y = selecao(pop);
+      if (fim != 0) { printf("Não foi possível determinar um horário.\n"); return pop[0]; }
       z = x.cruza(y);
-      z.muta(MAX - z.materias.size());
+      //printf("D\n");
+      z.muta(100);
+      //printf("E\n");
       novos[i] = z;
-      //if (z.erros <= err && z.materias.size() == disciplina.size()) return z;
+      //printf("F\n");
+      if (z.erros <= err && z.materias.size() == disciplina.size()) return z;
+      //printf("G\n");
     }
     for (int i = 0; i < TAM; i++) pop[i] = novos[i];
   }
+  //printf("H   %d\n", _contador);
   int menor = INF, pos = 0;
-  for (int i = 0; i < TAM; i++)
-    if (pop[i].erros < menor) { menor = pop[i].erros; pos = i; }
+  for (int i = 0; i < TAM; i++) {
+    //printf("\n-----------------------------------\n");
+    if (pop[i].erros <= menor && z.materias.size() == disciplina.size()) { menor = pop[i].erros; pos = i; }
+    //print(pop[i]);
+    //printf("-------------------------------------\n");
+  }
   return pop[pos];
 }
+
 
 int main(void) {
   in();
   debug();
   srand(time(NULL)); //Seed do rand
   individuo p = genetico(ERR);
+  print(p);
   return 0;
 }
